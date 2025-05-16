f21626d065c0c086e70b8e8cb8326fd8e5fec622
Huuhoang-1208 <hoang.nguyen230401@vnuk.edu.vn>
2025-05-16 19:05:55 +0700

    nop bai

--- src/ss1_tong_quan_java/bai_tap/hien_thi_loi_chao.java
+++ src/ss1_tong_quan_java/bai_tap/hien_thi_loi_chao.java
@@ -1,7 +1,18 @@
 package ss1_tong_quan_java.bai_tap;
+import java.util.Scanner;
 
 public class hien_thi_loi_chao {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        do {
+            System.out.println("Mời nhập tên của bạn: ");
+            name = scanner.nextLine();
+            if (!name.equals("Hoàng")) {
+                System.out.printf("Xin chào, %s!\n", name);
+            }else {
+                System.out.println("Chào:" + name);
+            }
+        } while (!name.equals("Hoàng"));
     }
 }
--- src/ss1_tong_quan_java/bai_tap/ung_dung_chuyen_doi_tien_te.java
+++ src/ss1_tong_quan_java/bai_tap/ung_dung_chuyen_doi_tien_te.java
@@ -1,4 +1,15 @@
 package ss1_tong_quan_java.bai_tap;
-
+import java.util.Scanner;
 public class ung_dung_chuyen_doi_tien_te {
+    public static void main (String [] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Mòi bạn nhập số tiền cần quy đổi : ");
+        usd = sc.nextDouble();;
+        double quydoi = usd * 23000;
+        System.out.print( "Giá trị VND" + quydoi);
+    }
 }
+
+
