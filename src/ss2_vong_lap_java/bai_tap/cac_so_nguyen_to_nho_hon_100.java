232ac5510fb31846f4c21acf945ccb450aeee21c
Huuhoang-1208 <hoang.nguyen230401@vnuk.edu.vn>
2025-05-19 18:47:27 +0700

    nop bai

--- src/ss2_vong_lap_java/bai_tap/cac_so_nguyen_to_nho_hon_100.java
+++ src/ss2_vong_lap_java/bai_tap/cac_so_nguyen_to_nho_hon_100.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 public class cac_so_nguyen_to_nho_hon_100 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        cac_so_nguyen_to_nho_hon_100 min = new cac_so_nguyen_to_nho_hon_100();
+        System.out.println("nhap cac so nguyen to nho hon 100 ");
+        for ( int i = 0; i < 100; i++){
+            if (min.checkSNT(i)){
+                System.out.println(i + " ");
+            }
+        }
 
     }
+
+    public boolean checkSNT(int n ) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+
