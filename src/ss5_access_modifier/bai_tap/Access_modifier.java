3dcd1dfe7a1734af1b410a8e4b5f297c61ea68ab
Huuhoang-1208 <hoang.nguyen230401@vnuk.edu.vn>
2025-05-26 19:08:56 +0700

    nop bai

--- src/ss5_access_modifier/bai_tap/Access_modifier.java
+++ src/ss5_access_modifier/bai_tap/Access_modifier.java
@@ -1,4 +1,26 @@
 package ss5_access_modifier.bai_tap;
 
 public class Access_modifier {
+    public static class Circle {
+        private double radius;
+        private String color;
+
+        public Circle(double radius, String color) {
+            this.radius = radius;
+        }
+
+        public Circle(int radius) {
+        }
+
+        public double getRadius() {
+            return radius;
+        }
+        public double getArea() {
+            return Math.PI * radius * radius;
+        }
+        @Override
+        public String toString() {
+            return "Circle [radius=" + radius + ", color=" + color + "]";
+        }
+    }
 }
--- src/ss5_access_modifier/bai_tap/test_circle.java
+++ src/ss5_access_modifier/bai_tap/test_circle.java
@@ -1,4 +1,10 @@
 package ss5_access_modifier.bai_tap;
 
 public class test_circle {
+    public static void main(String[] args) {
+        Access_modifier a = new Access_modifier();
+        Access_modifier.Circle c = new Access_modifier.Circle(10);
+        System.out.println(c.getArea());
+        System.out.println(c);
+    }
 }
